cfef1d07b4091e910510ff149f1453c46ddf5d74
Graziano Corineo <grazianocorineo01@gmail.com>
2023-06-01 21:31:43 +0200

    Exercise-NodeJs-03

---
+++ exercise-03.js
@@ -0,0 +1,5 @@
+
+
+import function01 from "./funcion01";
+
+function01();
---
+++ funcion01.js
@@ -0,0 +1,9 @@
+
+
+const x = 10;
+
+export default function function01() {
+    for (let i = 0; i <= x; i++) {
+       console.log(i);
+    }
+};
