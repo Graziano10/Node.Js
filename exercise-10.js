caed01404b21ed42108b38e58158cbb321207e67
Graziano Corineo <grazianocorineo01@gmail.com>
2023-06-15 20:07:59 +0200

    Exercise-NodJs-10

--- exercise-10.js
+++ exercise-10.js
@@ -1,30 +1,41 @@
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
 
 
-function luckyDraw(player) {
-    return new Promise((resolve, reject) => {
-      const win = Boolean(Math.round(Math.random()));
-  
-      process.nextTick(() => {
-        if (win) {
-          resolve(`${player} won a prize in the draw!`);
-        } else {
-          reject(new Error(`${player} lost the draw.`));
-        }
-      });
-    });
-  };
+const app = express();
+const port = process.env.PORT || 3000;
 
-const promisResult = async () => {
-    const players = ['Tina', 'Jorge', 'Julien'];
-  
-    try {
-      const results = await Promise.all(players.map(player => luckyDraw(player)));
-      results.forEach(result => {
-        console.log(result); 
-      });
-    } catch (err) {
-      console.error(err.message); 
-    }
-  }
-  
-  promisResult();
+app.use(express.json());
+
+app.use(morgan('dev'));
+
+let planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+app.get('/planets', (req, res) => {
+  res.json(planets);
+});
+
+app.post('/planets', (req, res) => {
+  const newPlanet = req.body;
+  planets.push(newPlanet);
+  res.status(201).json(newPlanet);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Server Error 1001' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
